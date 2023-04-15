3393749a432f9819a7a9f452dcd55142ef9454ef
OĞUZHAN MAVİ <mavioguz09@gmail.com>
2023-04-16 00:27:27 +0300

    springboot-backend

---
+++ webspring-chattapp/src/main/java/com/oguzhanmavi/webspringchattapp/WebspringChattappApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhanmavi.webspringchattapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspringChattappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspringChattappApplication.class, args);
+	}
+
+}
---
+++ webspring-chattapp/src/test/java/com/oguzhanmavi/webspringchattapp/WebspringChattappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhanmavi.webspringchattapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspringChattappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
